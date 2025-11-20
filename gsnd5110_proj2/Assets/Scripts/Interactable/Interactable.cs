980123f65954a619e1c7ab9134cbd6612319a493
Ray Hsiao <40257341+rh5140@users.noreply.github.com>
2025-11-19 23:58:03 -0800

    Add interactable indicator popup when player is in range

--- gsnd5110_proj2/Assets/Scripts/Interactable/Interactable.cs
+++ gsnd5110_proj2/Assets/Scripts/Interactable/Interactable.cs
@@ -2,8 +2,26 @@ using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
+    [SerializeField] GameObject indicator;
     public virtual void RunInteraction()
     {
         Debug.Log("RUNNING INTERACTION");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.name + " TRIGGER ENTERED");
+        if (other.gameObject.tag == "Player") 
+        {
+            if (indicator != null) indicator.SetActive(true);
+            else Debug.Log("Indicator for " + gameObject.name + " not found");
+        }
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log(other.gameObject.name + " TRIGGER EXITED");
+        if (indicator != null) indicator.SetActive(false);
+        else Debug.Log("Indicator for " + gameObject.name + " not found");
     }
 }
