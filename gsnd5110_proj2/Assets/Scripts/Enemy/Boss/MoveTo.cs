21c87f6cd24abfdbb59166fa1c0edb5319d3f66b
Ray Hsiao <40257341+rh5140@users.noreply.github.com>
2025-11-30 00:07:37 -0800

    Change boss implementation

--- gsnd5110_proj2/Assets/Scripts/Enemy/Boss/MoveTo.cs
+++ gsnd5110_proj2/Assets/Scripts/Enemy/Boss/MoveTo.cs
@@ -1,27 +1,27 @@
     // MoveTo.cs 
     // Modified from: https://docs.unity3d.com/Packages/com.unity.ai.navigation@2.0/manual/CreateNavMeshAgent.html
+    using System.Collections;
     using UnityEngine;
     using UnityEngine.AI;
 
     public class MoveTo : MonoBehaviour {
 
-       public Transform goal;
-       public float interval = 3f;
-       private float currTime = 0f;
-       NavMeshAgent agent;
+      public Transform goal;
+      public float interval = 3f;
+      private float currTime = 0f;
+      NavMeshAgent agent;
 
-       void Start () {
-          agent = GetComponent<NavMeshAgent>();
-          agent.destination = goal.position;
-       }
+      void Start () {
+         agent = GetComponent<NavMeshAgent>();
+      }
 
-       void Update()
-       {
-            if (currTime > interval)
-            {
-                agent.destination = goal.position;
-                currTime = 0;
-            }
-            currTime += Time.deltaTime;
-       }
+      public void MoveToTarget()
+      {
+         agent.SetDestination(goal.position);
+      }
+
+      public void StopMoving()
+      {
+         agent.ResetPath();
+      }
     }
