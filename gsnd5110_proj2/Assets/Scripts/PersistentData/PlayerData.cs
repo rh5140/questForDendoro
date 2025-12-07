c29943db7261c9a06ac4039fa8c2a10faa1f091a
Ray Hsiao <40257341+rh5140@users.noreply.github.com>
2025-12-06 23:32:42 -0800

    Create scaffolding for changing weapon, change fire import settings

--- gsnd5110_proj2/Assets/Scripts/PersistentData/PlayerData.cs
+++ gsnd5110_proj2/Assets/Scripts/PersistentData/PlayerData.cs
@@ -4,4 +4,5 @@ using UnityEngine;
 public class PlayerData : ScriptableObject
 {
     public ColorVariantAssets colorVariant;
+    public bool hasSword;
 }
--- gsnd5110_proj2/Assets/Scripts/PersistentData/PlayerSetup.cs
+++ gsnd5110_proj2/Assets/Scripts/PersistentData/PlayerSetup.cs
@@ -11,6 +11,9 @@ public class PlayerSetup : MonoBehaviour
     public SpriteRenderer r_arm;
     public SpriteRenderer dead;
 
+    public GameObject stickHand;
+    public GameObject swordHand;
+
     void Awake()
     {
         helm.sprite = playerData.colorVariant.helm;
@@ -19,5 +22,21 @@ public class PlayerSetup : MonoBehaviour
         body.sprite = playerData.colorVariant.body;
         r_arm.sprite = playerData.colorVariant.r_arm;
         dead.sprite = playerData.colorVariant.dead;
+
+        ChangeHand();
+    }
+
+    public void ChangeHand()
+    {
+        if (playerData.hasSword)
+        {
+            stickHand.SetActive(false);
+            swordHand.SetActive(true);
+        }
+        else
+        {
+            stickHand.SetActive(true);
+            swordHand.SetActive(false);
+        }
     }
 }
