e0e7bc10e84700544cdc5a1137e2de2f923a8ce9
Ray Hsiao <40257341+rh5140@users.noreply.github.com>
2025-12-07 19:07:36 -0800

    Make main menu buttons real

--- gsnd5110_proj2/Assets/Scripts/Interface/MainMenu.cs
+++ gsnd5110_proj2/Assets/Scripts/Interface/MainMenu.cs
@@ -2,8 +2,21 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip sfx;
+
+    public void ShowGameObject(GameObject go)
+    {
+        go.SetActive(true);
+    }
+
     public void HideGameObject(GameObject go)
     {
         go.SetActive(false);
+    }
+
+    public void PlaySfx()
+    {
+        audioSource.PlayOneShot(sfx);
     }
 }
